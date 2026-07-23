ebcc305fdd84d489d451b1ab56b978998d5c70b4
csj3814-create <csj3814@gmail.com>
2026-07-24 00:06:20 +0900

    Bump PWA cache version v249 -> v250 for release

--- js/diet-analysis.js
+++ js/diet-analysis.js
@@ -2,11 +2,11 @@
  * Client helpers for AI food, exercise, sleep/mind, blood-test, and step screenshot analysis.
  */
 
-import { auth, functions } from './firebase-config.js?v=249';
+import { auth, functions } from './firebase-config.js?v=250';
 import { httpsCallable } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-functions.js';
-import { showToast } from './ui-helpers.js?v=249';
-import { escapeHtml } from './security.js?v=249';
-import { getLocale, isEnglishLocale, t } from './i18n.js?v=249';
+import { showToast } from './ui-helpers.js?v=250';
+import { escapeHtml } from './security.js?v=250';
+import { getLocale, isEnglishLocale, t } from './i18n.js?v=250';
 
 const analyzeDietFn = httpsCallable(functions, 'analyzeDiet');
 const analyzeSleepMindFn = httpsCallable(functions, 'analyzeSleepMind');
--- js/i18n.js
+++ js/i18n.js
@@ -1,4 +1,4 @@
-import { buildLocalizedUrl, getLocale as getRouteLocale } from './app-mode.js?v=249';
+import { buildLocalizedUrl, getLocale as getRouteLocale } from './app-mode.js?v=250';
 
 const DEFAULT_LOCALE = 'ko';
 const ENGLISH_LOCALE = 'en';
