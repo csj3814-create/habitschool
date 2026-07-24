f2a62208f4e849fbd8411d4ac53996346ef6b92f
csj3814-create <csj3814@gmail.com>
2026-07-24 10:22:56 +0900

    Bump PWA cache version v251 -> v252 for release

--- js/i18n.js
+++ js/i18n.js
@@ -1,4 +1,4 @@
-import { buildLocalizedUrl, getLocale as getRouteLocale } from './app-mode.js?v=251';
+import { buildLocalizedUrl, getLocale as getRouteLocale } from './app-mode.js?v=252';
 
 const DEFAULT_LOCALE = 'ko';
 const ENGLISH_LOCALE = 'en';
--- js/reward-market.js
+++ js/reward-market.js
@@ -1,7 +1,7 @@
-import { auth, db, functions } from './firebase-config.js?v=251';
+import { auth, db, functions } from './firebase-config.js?v=252';
 import { doc, setDoc } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js';
 import { httpsCallable } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-functions.js';
-import { showToast } from './ui-helpers.js?v=251';
+import { showToast } from './ui-helpers.js?v=252';
 
 const REWARD_MARKET_CACHE_TTL = 30_000;
 const REWARD_MARKET_SNAPSHOT_TIMEOUT_MS = 7000;
