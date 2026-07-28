10b13871586c7576bf7b8d78649451c63ed3c117
csj3814-create <csj3814@gmail.com>
2026-07-28 22:52:55 +0900

    Bump PWA cache version v255 -> v256 for release

--- js/diet-analysis.js
+++ js/diet-analysis.js
@@ -2,11 +2,11 @@
  * Client helpers for AI food, exercise, sleep/mind, blood-test, and step screenshot analysis.
  */
 
-import { auth, functions } from './firebase-config.js?v=255';
+import { auth, functions } from './firebase-config.js?v=256';
 import { httpsCallable } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-functions.js';
-import { showToast } from './ui-helpers.js?v=255';
-import { escapeHtml } from './security.js?v=255';
-import { getLocale, isEnglishLocale, t } from './i18n.js?v=255';
+import { showToast } from './ui-helpers.js?v=256';
+import { escapeHtml } from './security.js?v=256';
+import { getLocale, isEnglishLocale, t } from './i18n.js?v=256';
 
 const analyzeDietFn = httpsCallable(functions, 'analyzeDiet');
 const analyzeSleepMindFn = httpsCallable(functions, 'analyzeSleepMind');
