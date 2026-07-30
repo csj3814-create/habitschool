8196bde74c3f95e3890dd116ecb0b6fc87eed41f
csj3814-create <csj3814@gmail.com>
2026-07-30 16:56:59 +0900

    Bump PWA cache version v262 -> v263 for release

--- js/i18n.js
+++ js/i18n.js
@@ -1,4 +1,4 @@
-import { buildLocalizedUrl, getLocale as getRouteLocale } from './app-mode.js?v=262';
+import { buildLocalizedUrl, getLocale as getRouteLocale } from './app-mode.js?v=263';
 
 const DEFAULT_LOCALE = 'ko';
 const ENGLISH_LOCALE = 'en';
